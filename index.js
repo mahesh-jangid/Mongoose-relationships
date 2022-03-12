8a9b342c523f2aa263cba5e778c5f44803a5e9b1
mahesh-jangid <mesh.jangid@gmail.com>
2022-03-12 23:51:11 +0530

    Completed

--- index.js
+++ index.js
@@ -7,16 +7,16 @@ app.use(express.json());
 
 const connect = () => {
   return mongoose.connect(
-    "mongodb+srv://Mahesh:m9024731575j@cluster0.t9tfc.mongodb.net/Library?retryWrites=true&w=majority"
+    "mongodb+srv://maheshJ:mahesh9000@cluster0.3yoo8.mongodb.net/web15_Library-system?retryWrites=true&w=majority"
   );
 };
 
 // SECTION SCHEMA
 
-const sectionSchema = new mongoose.Schema(
+const userSchema = new mongoose.Schema(
   {
     firstName: { type: String, required: true },
-    lastName: { type: String, required: false },
+    lastname: { type: String, required: true },
   },
   {
     versionKey: false,
@@ -25,6 +25,20 @@ const sectionSchema = new mongoose.Schema(
 );
 
 // Step 2 : creating the model
+const Users = mongoose.model("users", userSchema);
+
+// SECTION SCHEMA
+
+const sectionSchema = new mongoose.Schema(
+  {
+    Name: { type: String, required: true },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+);
+
 const Sections = mongoose.model("section", sectionSchema);
 
 // BOOK SCHEMA
@@ -46,12 +60,15 @@ const bookSchema = new mongoose.Schema(
 );
 
 const Books = mongoose.model("book", bookSchema);
-const CheckedOutSchema = new mongoose.Schema(
+
+// AUTHORS SCHEMA
+
+const AuthorSchema = new mongoose.Schema(
   {
-    BookId: {
+    userId: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: "book",
-      checkedOutTime: { type: Boolean, default: null },
+      ref: "users",
+      required: true,
     },
   },
   {
@@ -60,7 +77,74 @@ const CheckedOutSchema = new mongoose.Schema(
   }
 );
 
+const Authors = mongoose.model("author", AuthorSchema);
+
+// BOOk AUTHOR SCHEMA
+
+const BookAuthorSchema = new mongoose.Schema(
+  {
+    BookId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "book",
+      required: true,
+    },
+    AuthorId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "author",
+      required: true,
+    },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+);
+
+const BookAuthors = mongoose.model("bookauthor", BookAuthorSchema);
+
+// CHECKED OUT SCHEMA
+const CheckedOutSchema = new mongoose.Schema(
+  {
+    BookId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "book",
+      required: true,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "users",
+      required: true,
+    },
+    checkedOutTime: { type: Date, default: null },
+    checkedInTime: { type: Date, default: null },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+);
+
 const IsChecked = mongoose.model("checked", CheckedOutSchema);
+
+app.get("/users", async (req, res) => {
+  try {
+    const users = await Users.find().lean().exec();
+
+    return res.status(200).send({ users: users });
+  } catch (err) {
+    return res.status(500).send({ message: err.message });
+  }
+});
+
+app.post("/users", async (req, res) => {
+  try {
+    const users = await Users.create(req.body);
+
+    return res.status(201).send(users);
+  } catch (err) {
+    return res.status(500).send({ message: err.message });
+  }
+});
 
 app.get("/section", async (req, res) => {
   try {
@@ -100,11 +184,40 @@ app.post("/book", async (req, res) => {
     return res.status(500).send({ message: err.message });
   }
 });
-app.get("/ischecked/:BookId", async (req, res) => {
+app.get("/author", async (req, res) => {
   try {
-    const books = await Books.findById(req.params.BookId).lean().exec();
+    const books = await Books.find().lean().exec();
 
     return res.status(200).send({ books: books });
+  } catch (err) {
+    return res.status(500).send({ message: err.message });
+  }
+});
+
+app.post("/author", async (req, res) => {
+  try {
+    const books = await Books.create(req.body);
+
+    return res.status(201).send(books);
+  } catch (err) {
+    return res.status(500).send({ message: err.message });
+  }
+});
+
+app.get("/checked", async (req, res) => {
+  try {
+    const ischecked = await IsChecked.find().lean().exec();
+
+    return res.status(200).send({ ischecked: ischecked });
+  } catch (err) {
+    return res.status(500).send({ message: err.message });
+  }
+});
+app.get("/checked/:BookId", async (req, res) => {
+  try {
+    const books = await IsChecked.findById(req.params.BookId).lean().exec();
+
+    return res.status(200).send(books);
   } catch (err) {
     return res.status(500).send({ message: err.message });
   }
